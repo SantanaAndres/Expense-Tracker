04d1e9c5f38b8de7d3c6d64482c5c7800b37e586
SantanaAndres <pipe161102@outlook.com>
2026-09-07 20:16:31 -0500

    Add `GetAllFixedCostByUserResponse` DTO and update `FixedCostEndpoint` methods to use `NotImplementedException` stubs

--- app/ExpenseTrackerApi/Endpoint/FixedCost/FixedCostEndpoint.cs
+++ app/ExpenseTrackerApi/Endpoint/FixedCost/FixedCostEndpoint.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Dto.Request;
+using Application.Dto.Response.FixedCost;
 using Wolverine.Http;
 
 namespace ExpenseTrackerApi.Endpoint.FixedCost;
@@ -9,17 +10,17 @@ public class FixedCostEndpoint
     [Tags("FixedCosts")]
     [EndpointSummary("Get all fixed costs of user")]
     [EndpointDescription("Endpoint that give to the user all the fixed costs of him")]
-    public static string GetAllFixedByUserIdCosts(GetAllFixedCostOfUserRequest fixedCostRequest) => "Hola";
+    public static GetAllFixedCostByUserResponse GetAllFixedByUserIdCosts(GetAllFixedCostOfUserRequest fixedCostRequest) => throw new NotImplementedException();
     
     [WolverinePost("/add-fixed-cost")]
     [Tags("FixedCosts")]
     [EndpointSummary("Add new fixed cost")]
     [EndpointDescription("Endpoint that allow to add a new fixed cost")]
-    public static string AddNewFixedCost() => "Hola";
+    public static string AddNewFixedCost() => throw new NotImplementedException();
     
     [WolverinePut("/modify-fixed-cost")]
     [Tags("FixedCosts")]
     [EndpointSummary("Modify fixed cost")]
     [EndpointDescription("Endpoint that allow to modify a fixed cost")]
-    public static string ModifyFixedCost() => "Hola";
+    public static string ModifyFixedCost() => throw new NotImplementedException();
 }
---
+++ src/Application/Dto/Response/FixedCost/GetAllFixedCostByUserResponse.cs
@@ -0,0 +1,8 @@
+﻿using Application.Dto.Request;
+
+namespace Application.Dto.Response.FixedCost;
+
+public record GetAllFixedCostByUserResponse(
+    int Id, 
+    List<AmountExpensesRequest> AmountExpenses
+    );
