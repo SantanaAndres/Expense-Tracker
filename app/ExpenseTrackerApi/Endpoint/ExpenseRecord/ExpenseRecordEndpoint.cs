79bcc5085d13a8059c381586182f8469b92b9a1e
SantanaAndres <pipe161102@outlook.com>
2026-09-07 20:52:54 -0500

    Add `ExpenseRecordResponse` DTO and implement `AddNewExpenseRecord` and `ModifyExpenseRecord` endpoints

---
+++ app/ExpenseTrackerApi/Endpoint/ExpenseRecord/ExpenseRecordEndpoint.cs
@@ -0,0 +1,18 @@
+﻿using Wolverine.Http;
+
+namespace ExpenseTrackerApi.Endpoint.ExpenseRecord;
+
+public static class ExpenseRecordEndpoint
+{
+    [WolverinePost("/add-expense-record")]
+    [Tags("ExpenseRecord")]
+    [EndpointSummary("Add new expense record")]
+    [EndpointDescription("Endpoint that allow to add a new expense record")]
+    public static string AddNewExpenseRecord() => "Hola";
+    
+    [WolverinePut("/modify-expense-record")]
+    [Tags("ExpenseRecord")]
+    [EndpointSummary("Modify expense record")]
+    [EndpointDescription("Endpoint that allow to modify an expense record")]
+    public static string ModifyExpenseRecord() => "Hola";
+}
---
+++ src/Application/Dto/Response/ExpenseRecord/ExpenseRecordResponse.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities;
+
+namespace Application.Dto.Response.ExpenseRecord;
+
+public record ExpenseRecordResponse(
+    int Id,
+    AmountExpenses AmountExpenses,
+    DateTimeOffset  Date
+    );
