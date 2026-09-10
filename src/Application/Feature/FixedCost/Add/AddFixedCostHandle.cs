de08f601460cf81c618caec340637d3cbb7d3b97
SantanaAndres <pipe161102@outlook.com>
2026-09-09 20:07:05 -0500

    Add `AddFixedCostHandle` implementation with repository method and DTO conversion

--- src/Application/Feature/FixedCost/Add/AddFixedCostHandle.cs
+++ src/Application/Feature/FixedCost/Add/AddFixedCostHandle.cs
@@ -1,6 +1,17 @@
-﻿namespace Application.Feature.FixedCost.Add;
+﻿using Application.Abstraction.Repository;
+using Application.Dto.Response.FixedCost;
+using Application.Helper;
+
+namespace Application.Feature.FixedCost.Add;
 
 public class AddFixedCostHandle
 {
-    
+    public async Task<FixedCostByUserResponse> HandleAsync(
+        AddFixedCostCommand command,
+        IFixedCostRepository fixedCostRepository
+        )
+    {
+        var result = await fixedCostRepository.AddFixedCost(command);
+        return result.ToDto();
+    }
 }
