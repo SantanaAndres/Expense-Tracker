53946725264d89f4bb1c5ae2f8f0a2cd04080085
SantanaAndres <pipe161102@outlook.com>
2026-09-14 19:47:01 -0500

    Refactor `ExpenseTypeEndpoint` methods to include command and query definitions; implement `GetAllExpensesTypeHandle` handler with repository method and DTO conversion

--- app/ExpenseTrackerApi/Endpoint/ExpenseType/ExpenseTypeEndpoint.cs
+++ app/ExpenseTrackerApi/Endpoint/ExpenseType/ExpenseTypeEndpoint.cs
@@ -1,4 +1,9 @@
 ﻿using Application.Dto.Response.ExpenseTypes;
+using Application.Feature.ExpenseType.Add;
+using Application.Feature.ExpenseType.Get.GetAll;
+using Application.Feature.ExpenseType.Update;
+using Microsoft.AspNetCore.Mvc;
+using Wolverine;
 using Wolverine.Http;
 
 namespace ExpenseTrackerApi.Endpoint.ExpenseType;
@@ -9,17 +14,29 @@ public static class ExpenseTypeEndpoint
     [Tags("ExpenseTypes")]
     [EndpointSummary("Get all expense types")]
     [EndpointDescription("Get all expense types from DB")]
-    public static GetExpensesTypesResponse GetAllAllowedExpensesTypes() => throw new NotImplementedException();
-    
+    public static async Task<List<ExpenseTypeDataResponse>> GetAllAllowedExpensesTypes(IMessageBus bus)
+    {
+        var result = await bus.InvokeAsync<List<ExpenseTypeDataResponse>>(new GetAllExpensesTypeQuery());
+        return result;
+    }
+
     [WolverinePost("/add-expense-type")]
     [Tags("ExpenseTypes")]
     [EndpointSummary("Add new expense type")]
     [EndpointDescription("Add new expense type to DB")]
-    public static ExpenseTypeDataResponse AddNewExpenseType() => throw new NotImplementedException();
-    
+    public static async Task<ExpenseTypeDataResponse> AddNewExpenseType(IMessageBus bus, [FromBody] AddExpenseTypeCommand  command)
+    {
+        var result = await bus.InvokeAsync<ExpenseTypeDataResponse>(command);
+        return result;
+    }
+
     [WolverinePut("/modify-expense-type")]
     [Tags("ExpenseTypes")]
     [EndpointSummary("Modify expense type")]
     [EndpointDescription("Modify expense type to DB")]
-    public static ExpenseTypeDataResponse ModifyExpenseType() => throw new NotImplementedException();
+    public static async Task<ExpenseTypeDataResponse> ModifyExpenseType(IMessageBus bus, [FromBody] ModifyExpenseTypeCommand command)
+    {
+        var result = await bus.InvokeAsync<ExpenseTypeDataResponse>(command);
+        return result;
+    }
 }
---
+++ src/Application/Feature/ExpenseType/Get/GetAll/GetAllExpensesTypeHandle.cs
@@ -0,0 +1,22 @@
+﻿using Application.Abstraction.Repository;
+using Application.Dto.Response.ExpenseTypes;
+
+namespace Application.Feature.ExpenseType.Get.GetAll;
+
+public record GetAllExpensesTypeQuery;
+
+public class GetAllExpensesTypeHandle
+{
+    public async Task<List<ExpenseTypeDataResponse>> HandleAsync(GetAllExpensesTypeQuery query, IExpenseTypeRepository expenseTypeRepository)
+    {
+        var result = await expenseTypeRepository.GetAllExpenseTypes();
+
+        return result.Select( r =>
+            new ExpenseTypeDataResponse(
+                Id: r.ExpenseTypeId,
+                ExpenseTypeName: r.ExpenseName,
+                IsActive: r.IsActive
+                )
+            ).ToList();
+    }
+}
