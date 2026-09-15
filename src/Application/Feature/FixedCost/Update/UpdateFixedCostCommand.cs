bf5674879563c6f1e4125964edc1dcfc28f76dfb
SantanaAndres <pipe161102@outlook.com>
2026-09-14 19:30:41 -0500

    Refactor `UpdateFixedCostHandle` to include `UpdateFixedCostCommand` definition

--- src/Application/Feature/FixedCost/Update/UpdateFixedCostCommand.cs
+++
@@ -1,5 +0,0 @@
-﻿using Application.Dto.Request;
-
-namespace Application.Feature.FixedCost.Update;
-
-public record UpdateFixedCostCommand(int FixedCostId, List<AmountExpensesRequest> AmountExpenses);
--- src/Application/Feature/FixedCost/Update/UpdateFixedCostHandle.cs
+++ src/Application/Feature/FixedCost/Update/UpdateFixedCostHandle.cs
@@ -1,8 +1,11 @@
 ﻿using Application.Abstraction.Repository;
+using Application.Dto.Request;
 using Application.Dto.Response.FixedCost;
 using Application.Helper;
 
 namespace Application.Feature.FixedCost.Update;
+
+public record UpdateFixedCostCommand(int FixedCostId, List<AmountExpensesRequest> AmountExpenses);
 
 public class UpdateFixedCostHandle
 {
