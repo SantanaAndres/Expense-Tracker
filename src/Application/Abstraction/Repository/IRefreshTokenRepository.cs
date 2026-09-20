cf58527d2dd49bd4962d763deb5412a05870f4aa
SantanaAndres <pipe161102@outlook.com>
2026-09-20 14:46:20 -0500

    Add `IRefreshTokenRepository` interface and its implementation in `RefreshTokenRepository`.

---
+++ src/Application/Abstraction/Repository/IRefreshTokenRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Abstraction.Repository;
+
+public interface IRefreshTokenRepository
+{
+    Task AddAsync(CancellationToken cancellationToken);
+    
+    Task RevokeAsync(CancellationToken cancellationToken);
+    
+}
---
+++ src/Infrastructure/Persistence/Repositories/RefreshTokenRepository.cs
@@ -0,0 +1,16 @@
+﻿using Application.Abstraction.Repository;
+
+namespace Infrastructure.Persistence.Repositories;
+
+public class RefreshTokenRepository: IRefreshTokenRepository
+{
+    public Task AddAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public Task RevokeAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
