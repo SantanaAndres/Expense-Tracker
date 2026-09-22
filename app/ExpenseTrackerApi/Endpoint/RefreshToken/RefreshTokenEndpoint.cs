979a84b967df67ed691e5373a320aaa579861da9
SantanaAndres <pipe161102@outlook.com>
2026-09-22 18:51:28 -0500

    Add `RefreshTokenEndpoint` to provide a new token via `/get-new-token` API.

---
+++ app/ExpenseTrackerApi/Endpoint/RefreshToken/RefreshTokenEndpoint.cs
@@ -0,0 +1,23 @@
+﻿using Application.Dto.Response.RefreshToken;
+using Application.Feature.FixedCost.Get;
+using Application.Feature.RefreshToken;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Wolverine;
+using Wolverine.Http;
+
+namespace ExpenseTrackerApi.Endpoint.RefreshToken;
+
+public static class RefreshTokenEndpoint
+{
+    [WolverinePost("/get-new-token")]
+    [Tags("RefreshToken")]
+    [EndpointSummary("Get a new token")]
+    [EndpointDescription("Endpoint that give to the user a new token")]
+    [AllowAnonymous]
+    public static async Task<RefreshTokenResponse> RefreshAccessToken([FromBody] RefreshTokenCommand command, IMessageBus bus)
+    {
+        var result = await bus.InvokeAsync<RefreshTokenResponse>(command);
+        return result;
+    }
+}
