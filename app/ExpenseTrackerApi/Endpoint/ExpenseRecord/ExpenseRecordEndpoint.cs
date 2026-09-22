080399a52bbe09cc029698f342646ad28c1f822b
SantanaAndres <pipe161102@outlook.com>
2026-09-22 18:50:38 -0500

    Secure ExpenseRecord endpoints by adding `[Authorize]` attribute.

--- app/ExpenseTrackerApi/Endpoint/ExpenseRecord/ExpenseRecordEndpoint.cs
+++ app/ExpenseTrackerApi/Endpoint/ExpenseRecord/ExpenseRecordEndpoint.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Dto.Response.ExpenseRecord;
 using Application.Feature.ExpenseRecord.Add;
 using Application.Feature.ExpenseRecord.Update;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Wolverine;
 using Wolverine.Http;
@@ -13,6 +14,7 @@ public static class ExpenseRecordEndpoint
     [Tags("ExpenseRecord")]
     [EndpointSummary("Add new expense record")]
     [EndpointDescription("Endpoint that allow to add a new expense record")]
+    [Authorize]
     public static async Task<ExpenseRecordResponse> AddNewExpenseRecord(IMessageBus bus, [FromBody] AddExpenseRecordCommand command)
     {
         var result = await bus.InvokeAsync<ExpenseRecordResponse>(command);
@@ -23,6 +25,7 @@ public static class ExpenseRecordEndpoint
     [Tags("ExpenseRecord")]
     [EndpointSummary("Modify expense record")]
     [EndpointDescription("Endpoint that allow to modify an expense record")]
+    [Authorize]
     public static async Task<ExpenseRecordResponse> ModifyExpenseRecord(IMessageBus bus, [FromBody] UpdateExpenseRecordCommand command)
     {
         var result = await bus.InvokeAsync<ExpenseRecordResponse>(command);
