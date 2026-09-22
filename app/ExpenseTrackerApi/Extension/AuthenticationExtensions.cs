7684c70c0bd809d0cc7af5fc8b8414caf51d5630
SantanaAndres <pipe161102@outlook.com>
2026-09-22 18:50:25 -0500

    Add rate limiting for login attempts with fixed window policy and rejection handling

--- app/ExpenseTrackerApi/Extension/AuthenticationExtensions.cs
+++ app/ExpenseTrackerApi/Extension/AuthenticationExtensions.cs
@@ -1,5 +1,8 @@
 ﻿using System.Text;
+using System.Threading.RateLimiting;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
 using Microsoft.IdentityModel.Tokens;
 
 namespace ExpenseTrackerApi.Extension;
@@ -35,12 +38,37 @@ public static class AuthenticationExtensions
                 .RequireAuthenticatedUser()
                 .Build();
         });
+        
+        services.AddRateLimiter(options =>
+        {
+            options.AddFixedWindowLimiter("LoginLimit", opt =>
+            {
+                opt.PermitLimit = 5;
+                opt.Window = TimeSpan.FromMinutes(1);
+                opt.QueueLimit = 0;
+            });
+            
+            options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+            
+            options.OnRejected = async (context, token) =>
+            {
+                var problemDetails = new ProblemDetails
+                {
+                    Status = StatusCodes.Status429TooManyRequests,
+                    Title = "Too Many Requests",
+                    Detail = "Too many login attempts. Please wait 1 minute."
+                };
+                await context.HttpContext.Response.WriteAsJsonAsync(problemDetails, token);
+            };
+        });
+        
             
         return services;
     }
     
     public static WebApplication UseCustomAuthentication(this WebApplication app)
     {
+        app.UseRateLimiter();
         app.UseAuthentication();
         app.UseAuthorization();
         
