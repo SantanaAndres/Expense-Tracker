f2bb1e27b3a32739f440a7e10975ab6579443199
SantanaAndres <pipe161102@outlook.com>
2026-09-22 21:35:07 -0500

    Add `GetUserId` extension method for `ClaimsPrincipal` to retrieve user ID with validation

---
+++ app/ExpenseTrackerApi/Extension/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Claims;
+
+namespace ExpenseTrackerApi.Extension;
+
+public static class ClaimsPrincipalExtensions
+{
+    public static int GetUserId(this ClaimsPrincipal user)
+    {
+        var userId = user.FindFirstValue("UserId");
+        
+        if (string.IsNullOrEmpty(userId))
+            throw new UnauthorizedAccessException("User ID not found in claims.");
+        
+        return int.Parse(userId);
+        
+    }
+}
