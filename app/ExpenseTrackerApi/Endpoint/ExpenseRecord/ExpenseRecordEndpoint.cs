244f6dfbc23b7b5ea14b356d557f8d8deca2a7b3
SantanaAndres <pipe161102@outlook.com>
2026-09-23 19:42:45 -0500

    Refactor `ExpenseRecordEndpoint` to use request models and extract `userId` from `ClaimsPrincipal` for improved clarity and user association

--- app/ExpenseTrackerApi/Endpoint/ExpenseRecord/ExpenseRecordEndpoint.cs
+++ app/ExpenseTrackerApi/Endpoint/ExpenseRecord/ExpenseRecordEndpoint.cs
@@ -1,6 +1,9 @@
-﻿using Application.Dto.Response.ExpenseRecord;
+﻿using System.Security.Claims;
+using Application.Dto.Request;
+using Application.Dto.Response.ExpenseRecord;
 using Application.Feature.ExpenseRecord.Add;
 using Application.Feature.ExpenseRecord.Update;
+using ExpenseTrackerApi.Extension;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Wolverine;
@@ -10,25 +13,41 @@ namespace ExpenseTrackerApi.Endpoint.ExpenseRecord;
 
 public static class ExpenseRecordEndpoint
 {
+    public record AddExpenseRecord(AmountExpensesRequest AmountExpenses, DateTimeOffset Date);
     [WolverinePost("/add-expense-record")]
     [Tags("ExpenseRecord")]
     [EndpointSummary("Add new expense record")]
     [EndpointDescription("Endpoint that allow to add a new expense record")]
     [Authorize]
-    public static async Task<ExpenseRecordResponse> AddNewExpenseRecord(IMessageBus bus, [FromBody] AddExpenseRecordCommand command)
+    public static async Task<ExpenseRecordResponse> AddNewExpenseRecord(
+        IMessageBus bus, 
+        ClaimsPrincipal claims,
+        [FromBody] AddExpenseRecord request
+        )
     {
+        var command = new AddExpenseRecordCommand(claims.GetUserId(),request.AmountExpenses, request.Date);
+        
         var result = await bus.InvokeAsync<ExpenseRecordResponse>(command);
+        
         return result;
     }
 
+    public record UpdateExpenseRecord(int ExpenseRecordId, AmountExpensesRequest AmountExpenses, DateTimeOffset Date);
     [WolverinePut("/modify-expense-record")]
     [Tags("ExpenseRecord")]
     [EndpointSummary("Modify expense record")]
     [EndpointDescription("Endpoint that allow to modify an expense record")]
     [Authorize]
-    public static async Task<ExpenseRecordResponse> ModifyExpenseRecord(IMessageBus bus, [FromBody] UpdateExpenseRecordCommand command)
+    public static async Task<ExpenseRecordResponse> ModifyExpenseRecord(
+        IMessageBus bus, 
+        [FromBody] UpdateExpenseRecord request,
+        ClaimsPrincipal claims 
+        )
     {
+        var command = new UpdateExpenseRecordCommand(request.ExpenseRecordId, claims.GetUserId(),request.AmountExpenses, request.Date);
+        
         var result = await bus.InvokeAsync<ExpenseRecordResponse>(command);
+        
         return result;
     }
 }
